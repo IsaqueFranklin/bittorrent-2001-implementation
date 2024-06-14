9e3d6622c4b8d22f7c7a4b27193d408379e82cfe
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-06-13 22:04:59 -0300

    undertanding

--- main.go
+++ main.go
@@ -29,3 +29,36 @@ func Open(r io.Reader) (*bencodeTorrent, error) {
 
   return &bto, nil
 }
+
+type TorrentFile struct {
+  Announce string
+  infoHash [20]byte
+  PieceHashes [][20]byte
+  PieceLength int
+  Length int
+  Name string
+}
+
+func (bto bencodeTorrent) toTorrentFile() (TorrentFile, error){
+
+}
+
+func (t *TorrentFile) buildTrackerURL(peerID [20]byte, port uint16)(string, error){
+  base, err := url.Parse(t.Announce)
+
+  if err != nil {
+    return "", err
+  }
+
+  params := url.Values{
+    "info_hash":  []string{string(t.InfoHash[:])},
+    "peer_id":    []string{string(peerID[:])},
+    "port":       []string{strconv.Itoa(int(Port))},
+    "uploaded":   []string{"0"},
+    "downloaded": []string{"0"},
+    "compact":    []string{"1"},
+    "left":       []string{strconv.Itoa(t.Length)},
+  }
+  base.RawQuery = params.Encode()
+  return base.String(), nil
+}
